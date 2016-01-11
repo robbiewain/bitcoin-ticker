505e5145cad319b11f584f26e9be0fe051ed0654
Franck Verrot <franck.verrot@omadahealth.com>
2016-01-11 08:01:16 -0800

    Extract environment variables out of the ticker

--- lib/bitcoin_ticker.rb
+++ lib/bitcoin_ticker.rb
@@ -6,14 +6,28 @@ require "bitcoin_ticker/slack_notifier"
 
 module BitcoinTicker
   class BitcoinTicker
+    # call-seq:
+    #   BitcoinTicker.new price_threshold, slack_webhook_url
+    #
+    # Parameters:
+    #   * price_threshold   [Float] : Minimum variation of the BTC price before considering communicating it
+    #   * slack_webhook_url [String]: URL of the Slack Webhook
+    def initialize(price_threshold: ENV["BITCOIN_PRICE_THRESHOLD"].to_f, slack_webhook_url: ENV["SLACK_WEBHOOK_URL"])
+      self.price_threshold = price_threshold
+      self.slack_webhook_url = slack_webhook_url
+    end
+
     def tick
       current_price = PriceChecker.new.current_price
       price_saver = PriceSaver.new
       previous_price = price_saver.read
-      if PriceComparer.new.compare_to_threshold previous_price, current_price, ENV["BITCOIN_PRICE_THRESHOLD"].to_f
-        SlackNotifier.new.notify ENV["SLACK_WEBHOOK_URL"], current_price, current_price > previous_price
+      if PriceComparer.new.compare_to_threshold previous_price, current_price, price_threshold
+        SlackNotifier.new.notify slack_webhook_url, current_price, current_price > previous_price
         price_saver.write current_price
       end
     end
+
+    private
+    attr_accessor :price_threshold, :slack_webhook_url
   end
 end
