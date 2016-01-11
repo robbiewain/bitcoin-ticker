cc5389513bcf2cba8528da34da25a3d00bf6c740
Franck Verrot <franck.verrot@omadahealth.com>
2016-01-11 08:38:11 -0800

    Extract the API call + JSON parsing out of the `current_price` method

--- lib/bitcoin_ticker/price_checker.rb
+++ lib/bitcoin_ticker/price_checker.rb
@@ -12,16 +12,22 @@ module BitcoinTicker
     end
 
     def current_price
-      begin
-        res = Net::HTTP.get_response(coinbase_spot_price_uri)
-        json = JSON.parse(res.body)
-        json["data"]["amount"].to_f
-      rescue Exception => e
-        raise PriceChecker, "[PriceChecker#current_price] Error: #{e.message}"
-      end
+      price = read_json(coinbase_spot_price_uri)
+
+      price || (raise PriceChecker::Error, "[PriceChecker#current_price] Error: #{e.message}")
     end
 
     private
     attr_accessor :coinbase_spot_price_uri
+
+    def read_json(uri)
+      begin
+        res  = Net::HTTP.get_response(uri)
+        json = JSON.parse(res.body)
+        json["data"]["amount"].to_f
+      rescue Exception => _
+        nil
+      end
+    end
   end
 end
