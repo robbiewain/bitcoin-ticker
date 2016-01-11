46f7eb40c666596917d5c62f5155f59395801c52
Franck Verrot <franck.verrot@omadahealth.com>
2016-01-11 08:22:53 -0800

    Make PriceChecker configurable
    This commit also makes sure that a failed call to `current_price` raises
    an exception as we do not expect the program to continue running in such
    occasions.

--- lib/bitcoin_ticker.rb
+++ lib/bitcoin_ticker.rb
@@ -14,14 +14,15 @@ module BitcoinTicker
     #   * price_threshold   [Float] : Minimum variation of the BTC price before considering communicating it
     #   * slack_webhook_url [String]: URL of the Slack Webhook
     def initialize(price_threshold: ENV["BITCOIN_PRICE_THRESHOLD"].to_f, slack_webhook_url: ENV["SLACK_WEBHOOK_URL"])
-      self.price_threshold = price_threshold
+      self.price_threshold   = price_threshold
       self.slack_webhook_url = slack_webhook_url
-      self.notifier = SlackNotifier.new(slack_webhook_url)
-      self.price_saver = PriceSaver.new
+      self.notifier          = SlackNotifier.new(slack_webhook_url)
+      self.price_saver       = PriceSaver.new
+      self.price_checker     = PriceChecker.new
     end
 
     def tick
-      current_price = PriceChecker.new.current_price
+      current_price = price_checker.current_price
       previous_price = price_saver.read
 
       if compare_to_threshold previous_price, current_price, price_threshold
@@ -31,6 +32,6 @@ module BitcoinTicker
     end
 
     private
-    attr_accessor :price_threshold, :notifier, :price_saver
+    attr_accessor :price_threshold, :notifier, :price_saver, :price_checker
   end
 end
--- lib/bitcoin_ticker/price_checker.rb
+++ lib/bitcoin_ticker/price_checker.rb
@@ -3,16 +3,25 @@ require "json"
 
 module BitcoinTicker
   class PriceChecker
+    Error = Class.new(StandardError)
+
     COINBASE_SPOT_PRICE_URI = "https://api.coinbase.com/v2/prices/spot"
+
+    def initialize(coinbase_spot_price_uri: COINBASE_SPOT_PRICE_URI)
+      self.coinbase_spot_price_uri = URI(coinbase_spot_price_uri)
+    end
 
     def current_price
       begin
-        res = Net::HTTP.get_response(URI(COINBASE_SPOT_PRICE_URI))
+        res = Net::HTTP.get_response(coinbase_spot_price_uri)
         json = JSON.parse(res.body)
         json["data"]["amount"].to_f
       rescue Exception => e
-        puts e.message
+        raise PriceChecker, "[PriceChecker#current_price] Error: #{e.message}"
       end
     end
+
+    private
+    attr_accessor :coinbase_spot_price_uri
   end
 end
